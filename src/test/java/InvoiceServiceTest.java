f7a50c25b38069502cdd45def0072d2407c288de
sayalipawade <sayalipawade05@gmail.com>
2020-05-01 18:25:53 +0530

    Refactored:Compare with minimum fare

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare()
-    {
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator generator=new InvoiceGenerator();
         double distance=2.0;
         int time=5;
         double fare=generator.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0.0);
     }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare(){
+        InvoiceGenerator generator=new InvoiceGenerator();
+        double distance=0.1;
+        int time=1;
+        double fare=generator.calculateFare(distance,time);
+        Assert.assertEquals(5.0,fare,0.0);
+    }
+
 }
