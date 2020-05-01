e7a6bac71caf7fa3469142dc0cd2083f5e2ad6ee
sayalipawade <sayalipawade05@gmail.com>
2020-05-01 17:33:41 +0530

    Added:Welcome message

---
+++ src/main/java/com/cabservicegenerator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.cabservicegenerator;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator");
+    }
+}
