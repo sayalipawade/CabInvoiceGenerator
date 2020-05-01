6da0bae91d03897a709a2c0a7332088a6a9ff1ff
sayalipawade <sayalipawade05@gmail.com>
2020-05-01 18:00:38 +0530

    Added:Calculate total fare of journey

--- src/main/java/com/cabservicegenerator/InvoiceGenerator.java
+++ src/main/java/com/cabservicegenerator/InvoiceGenerator.java
@@ -4,4 +4,11 @@ public class InvoiceGenerator {
     public static void main(String[] args) {
         System.out.println("Welcome to cab invoice generator");
     }
+
+    private static final int COST_PER_TIME=1;
+    private static final double MINIMUM_COST_PER_KILOMETER=10;
+
+    public double calculateFare(double distance,int time) {
+        return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+    }
 }
---
+++ src/test/java/InvoiceServiceTest.java
@@ -0,0 +1,15 @@
+import com.cabservicegenerator.InvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare()
+    {
+        InvoiceGenerator generator=new InvoiceGenerator();
+        double distance=2.0;
+        int time=5;
+        double fare=generator.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+}
