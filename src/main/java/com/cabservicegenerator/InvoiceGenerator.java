445cd6482d09aeff0cc6bdb54fc054e0c3f14d49
sayalipawade <sayalipawade05@gmail.com>
2020-05-02 18:54:41 +0530

    Added:Calculating total fare for multiple rides

--- src/main/java/com/cabservicegenerator/InvoiceGenerator.java
+++ src/main/java/com/cabservicegenerator/InvoiceGenerator.java
@@ -2,21 +2,21 @@ package com.cabservicegenerator;
 
 public class InvoiceGenerator {
 
-    //main method
+    /**main method*/
     public static void main(String[] args) {
         System.out.println("Welcome to cab invoice generator");
     }
 
-    //Constants
+    /**Constants*/
     private static final int COST_PER_TIME=1;
     private static final double MINIMUM_COST_PER_KILOMETER=10;
 
-    //Method to calculate total fare
+    /**Method to calculate total fare*/
     public double calculateFare(double distance,int time) {
         return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
     }
 
-    //Method to calculate total fare for multiple rides
+    /**Method to calculate total fare for multiple rides*/
     public double calculateFare(Ride[] rides) {
         double totalFare=0;
         for(Ride ride:rides) {
