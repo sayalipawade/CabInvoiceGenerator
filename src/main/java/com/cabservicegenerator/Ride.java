ef1a5fbb2c722a774d9587147fb8da9bae39809f
sayalipawade <sayalipawade05@gmail.com>
2020-05-02 18:42:59 +0530

    Added:Calculating total fare for multiple rides

---
+++ src/main/java/com/cabservicegenerator/Ride.java
@@ -0,0 +1,14 @@
+package com.cabservicegenerator;
+
+public class Ride {
+
+    /**variables*/
+    public final double distance;
+    public final int time;
+
+    /**parameterized constructor*/
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,8 +1,11 @@
 import com.cabservicegenerator.InvoiceGenerator;
+import com.cabservicegenerator.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+
+    /** Given distance and time should return total fare*/
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator generator=new InvoiceGenerator();
@@ -12,13 +15,25 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25,fare,0.0);
     }
 
+    /** Given distance and time if total fare is less than minimum fare should return minimum fare */
     @Test
     public void givenDistanceAndTime_ShouldReturnMinimumFare(){
         InvoiceGenerator generator=new InvoiceGenerator();
-        double distance=0.1;
+        double distance=0.3;
         int time=1;
         double fare=generator.calculateFare(distance,time);
         Assert.assertEquals(5.0,fare,0.0);
+    }
+
+    /** Given multiple rides should return total fare */
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        InvoiceGenerator generator=new InvoiceGenerator();
+        Ride[] rides={  new Ride(2.0,5),
+                        new Ride(0.1,1)
+                    };
+        double fare=generator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
     }
 
 }
